3176f9083082ac452300016de7af608aeee15ed7
yizo <ql3895619@gmail.com>
2022-01-31 15:43:30 +0800

    first commit

---
+++ src/main.js
@@ -0,0 +1,19 @@
+import { createApp } from 'vue'
+import { Quasar } from 'quasar'
+
+// Import icon libraries
+import '@quasar/extras/material-icons/material-icons.css'
+
+// Import Quasar css
+import 'quasar/src/css/index.sass'
+
+import App from './App.vue'
+import router from './router'
+
+const app = createApp(App)
+
+app.use(router).use(Quasar, {
+  plugins: {}, // import Quasar plugins and add here
+})
+
+app.mount('#app')
---
+++ src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup>
+import TheWelcome from '@/components/TheWelcome.vue'
+</script>
+
+<template>
+  <main>
+    <TheWelcome />
+    <q-btn color="white" text-color="black" label="Standard" />
+  </main>
+</template>
