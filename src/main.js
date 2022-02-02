a1abd83e208a57f9571ecefcfcce626a3af21061
yizo <ql3895619@gmail.com>
2022-02-02 23:17:08 +0800

    Chore: add tailwind.css

--- src/main.js
+++ src/main.js
@@ -1,11 +1,9 @@
 import { createApp } from 'vue'
 import { Quasar } from 'quasar'
 
-// Import icon libraries
 import '@quasar/extras/material-icons/material-icons.css'
-
-// Import Quasar css
 import 'quasar/src/css/index.sass'
+import './index.css'
 
 import App from './App.vue'
 import router from './router'
