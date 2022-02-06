d2309e1ffda1c44a5b777c61fc62550c9489743e
yizo <ql3895619@gmail.com>
2022-02-07 02:25:08 +0800

    Feat: read image from local

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,24 +6,53 @@ import { ref } from '@vue/reactivity'
 import { useRouter } from 'vue-router'
 
 const route = useRouter()
-const buttonShown = ref(true)
+
+const imgInput = ref(null)
+const previewImage = ref('')
+
+const selectImg = () => {
+  imgInput.value.click()
+}
+
+const readImg = (e) => {
+  const files = e.target.files
+  if (files && files[0]) {
+    let reader = new FileReader()
+    reader.onload = (e) => {
+      previewImage.value = e.target.result
+    }
+    reader.readAsDataURL(files[0])
+  }
+}
 
 const uploadImg = async () => {
-  buttonShown.value = false
   const { photoId } = (await setPhoto()).data
   route.push(PATH.PHOTO.replace(':id', photoId))
-  // buttonShown.value = true
 }
 </script>
 
 <template>
   <div class="w-full flex flex-col items-center">
     <HelloWorld msg="You did it!" />
+    <img v-if="previewImage" :src="previewImage" class="w-64" />
+    <q-card v-else class="my-card h-64 w-64 bg-grey-4"> </q-card>
+    <input
+      ref="imgInput"
+      @change="readImg"
+      type="file"
+      accept="image/*"
+      hidden
+    />
     <q-btn
-      v-if="buttonShown"
       color="primary"
       class="q-mt-md w-64"
-      label="upload"
+      label="select image"
+      @click="selectImg"
+    />
+    <q-btn
+      color="primary"
+      class="q-mt-md w-64"
+      label="upload image"
       @click="uploadImg"
     />
   </div>
--- src/views/PhotoView.vue
+++ src/views/PhotoView.vue
@@ -3,7 +3,6 @@ import HelloWorld from '@/components/HelloWorld.vue'
 import { useRoute } from 'vue-router'
 
 const route = useRoute()
-console.log(route)
 const { id } = route.params
 const msg = `I will show you photo with id: ${id}`
 </script>
